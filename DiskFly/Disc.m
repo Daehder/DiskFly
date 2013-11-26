1786e8347a410484a8b0e54fa858308d1c151a4c
Austin Graham <austingraham@pcp072492pcs.wireless.calpoly.edu>
2013-11-26 10:02:09 -0800

    adsufohsaidf

--- DiskFly/Disc.m
+++ DiskFly/Disc.m
@@ -13,7 +13,7 @@
 @property CGPoint firstTouchLocation;
 @property CGPoint startPosition;
 @property NSDate *startTime;
-@property CGPoint *startPosition;
+//@property CGPoint *startPosition;
 @end
 
 @implementation Disc
@@ -95,7 +95,7 @@
 }
 
 -(void) resetDisc {
-    self.position = *(self.startPosition);
+    self.position = self.startPosition;
 }
 
 -(void) deleteDisc
