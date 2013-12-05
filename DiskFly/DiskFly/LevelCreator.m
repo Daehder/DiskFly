4feb8876e45cbcefc2d1dc71c42b6117d590b55d
Austin Graham <austingraham@Jamess-MacBook-Pro.local>
2013-12-04 17:27:01 -0800

    levels 7, and 8 done

--- DiskFly/DiskFly/LevelCreator.m
+++ DiskFly/DiskFly/LevelCreator.m
@@ -28,17 +28,7 @@
 {
     // Each Array represents a level
     // Create more Arrays for additional levels, then combine into an array
-    NSArray *l1 = @[ @{@"type": @"rect", @"x":@0, @"y":@400},
-                     @{@"type": @"rect", @"x":@300, @"y":@400},
-                     @{@"type": @"circle", @"x":@150, @"y":@400}];
-   // NSArray *l1 = @[ @{@"type": @"rect", @"x":@100, @"y":@400},
-     //                @{@"type": @"rect", @"x":@150, @"y":@400},
-       //              @{@"type": @"rect", @"x":@50, @"y":@400},
-         //            @{@"type": @"rect", @"x":@200, @"y":@400},
-           //          @{@"type": @"rect", @"x":@225, @"y":@400},
-             //        @{@"type": @"circle", @"x":@160, @"y":@200}];
-    
-    NSArray *l1 = @[ @{@"type": @"circle", @"x":@150, @"y":@300}];
+    NSArray *l1 = @[ @{@"type": @"circle", @"x":@160, @"y":@300}];
     
     NSArray *l2 = @[ @{@"type": @"rect", @"x":@0, @"y":@400},
                      @{@"type": @"rect", @"x":@300, @"y":@400},
@@ -60,13 +50,22 @@
                      @{@"type": @"rect", @"x":@300, @"y":@400},
                      @{@"type": @"circle", @"x":@160, @"y":@300}];
     
-    NSArray *l7 = @[ @{@"type": @"rect", @"x":@0, @"y":@400},
-                     @{@"type": @"rect", @"x":@300, @"y":@400},
-                     @{@"type": @"circle", @"x":@160, @"y":@300}];
+    NSArray *l7 = @[ @{@"type": @"rect", @"x":@50, @"y":@200},
+                     @{@"type": @"rect", @"x":@100, @"y":@300},
+                     @{@"type": @"rect", @"x":@150, @"y":@400},
+                     @{@"type": @"rect", @"x":@125, @"y":@350},
+                     @{@"type": @"rect", @"x":@75, @"y":@250},
+                     @{@"type": @"rect", @"x":@275, @"y":@400},
+                     @{@"type": @"circle", @"x":@160, @"y":@200}];
     
-    NSArray *l8 = @[ @{@"type": @"rect", @"x":@0, @"y":@400},
-                     @{@"type": @"rect", @"x":@300, @"y":@400},
-                     @{@"type": @"circle", @"x":@160, @"y":@300}];
+    NSArray *l8 = @[ @{@"type": @"rect", @"x":@25, @"y":@425},
+                     @{@"type": @"rect", @"x":@150, @"y":@310},
+                     @{@"type": @"rect", @"x":@150, @"y":@350},
+                     @{@"type": @"rect", @"x":@150, @"y":@270},
+                     @{@"type": @"rect", @"x":@300, @"y":@175},
+                     @{@"type": @"rect", @"x":@25, @"y":@175},
+                     @{@"type": @"rect", @"x":@300, @"y":@425},
+                     @{@"type": @"circle", @"x":@160, @"y":@150}];
     
     NSArray *l9 = @[ @{@"type": @"rect", @"x":@100, @"y":@400},
                      @{@"type": @"rect", @"x":@150, @"y":@400},
